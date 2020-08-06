534ae213517b5eba37da7c04c375fd1a94f66683
Jordan <jordandevaney28@gmail.com>
2020-08-06 19:49:08 -0400

    passed searching algorithms

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
+linear_search([-2, 7, 3, -9, 5, 1, 0, 4, -6], 0)
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    # get the middle index of the array
+    high = len(arr) - 1
+    while low <= high:
+        # search starting at the middle of the array
+        middle_index = int((low + high) / 2)
+        guess = arr[middle_index]
+        if guess == target:
+            return middle_index
+        if guess > target:
+            high = middle_index - 1
+        else:
+            low = middle_index + 1            
+    return -1
 
-    # Your code here
-
-
-    return -1  # not found
+binary_search([-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9], 9)
